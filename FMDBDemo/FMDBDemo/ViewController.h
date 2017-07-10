125746a1394cd4eb0944a8d9cb82c995bc4024b1
xinghunm <xinghunm@curacloudcorp.com>
2017-07-10 12:02:11 +0800

    Init the repository

---
+++ FMDBDemo/FMDBDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FMDBDemo
+//
+//  Created by xinghun meng on 10/07/2017.
+//  Copyright © 2017 xinghun meng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FMDBDemo/FMDBDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FMDBDemo
+//
+//  Created by xinghun meng on 10/07/2017.
+//  Copyright © 2017 xinghun meng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
